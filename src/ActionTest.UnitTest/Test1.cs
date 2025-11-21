46abc9180ed7b10ee7e8a7cb1a3129064aeab337
jason_yang <jason_yang@protech.com.tw>
2025-11-21 13:34:07 +0800

    feat: add UnitTest

---
+++ src/ActionTest.UnitTest/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace ActionTest.UnitTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
